086f1b2a242167dd5cffd47667c2ef7b50a5b2bf
Artem Dunaev <artem.dunaev@dualbootpartners.com>
2021-11-08 22:28:52 +0500

    fix rubocop

--- app/helpers/post_comments_helper.rb
+++ app/helpers/post_comments_helper.rb
@@ -2,8 +2,8 @@
 
 module PostCommentsHelper
   def nested_comments(comments)
-    comments.map do |comment, sub_comments|
+    safe_join(comments.map do |comment, sub_comments|
       render(comment) + content_tag(:div, nested_comments(sub_comments), class: 'nested_comments')
-    end.join.html_safe
+    end)
   end
 end
